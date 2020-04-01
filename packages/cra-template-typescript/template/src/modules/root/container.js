532fedd48c8b9c2c4f41e515f5d497f450572070
Miguel Cardoso <miguelmarquescardoso@gmail.com>
2020-04-01 15:10:44 +0100

    added navbar

--- packages/cra-template-typescript/template/src/modules/root/container.js
+++ packages/cra-template-typescript/template/src/modules/root/container.js
@@ -1,21 +1,44 @@
-import React  from 'react';
+import React, { Fragment }  from 'react';
 import { connect } from 'react-redux';
 import { Grid, Row } from 'react-bootstrap';
 import { Route, Switch } from 'react-router';
+import LoginNavBar from 'bananasplit/dist/components/navbar/login-navbar';
+import { NavbarStyled } from 'bananasplit/dist/styles/global/collapsed-navbar-base';
+
 import SimpleModule from '../../modules/simple-module';
+import { BootstrapStyled } from '../../styles/bootstrap'
+// import { ComponentsStyled } from '../../styles/components';
+// import { ResetStyled } from '../../styles/reset'; 
 
 export class RootContainer extends React.Component {
-    render(){
-        return (
-              <Grid fluid>
-                  <Row>
-                      <Switch>
-                          <Route path="/" component={SimpleModule} />
-                      </Switch>
-                  </Row>
-              </Grid>
-        );
-    }
+
+  // Import CSS reset and Global Styles
+  loadGlobalStyles() {
+    return (
+      <Fragment>
+        <BootstrapStyled />
+        {/* <ResetStyled /> */}
+        {/* <ComponentsStyled /> */}
+        <NavbarStyled />
+      </Fragment>
+    );
+  }
+
+  render(){
+    return (
+      <Fragment>
+        {this.loadGlobalStyles()}
+        <LoginNavBar />
+        <Grid fluid>
+          <Row>
+            <Switch>
+              <Route path="/" component={SimpleModule} />
+            </Switch>
+          </Row>
+        </Grid>
+      </Fragment>
+    );
+  }
 }
 
 const mapStateToProps = () => ({});
